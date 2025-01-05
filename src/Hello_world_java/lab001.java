85c50788646e37bd510b6b433a2bf9984b2303b3
subodh2499 <sdpatil2405@gmail.com>
2025-01-05 12:39:19 +0530

    k

---
+++ src/Hello_world_java/lab001.java
@@ -0,0 +1,8 @@
+package Hello_world_java;
+
+public class lab001 {
+    public static void main(String[] args) {
+        System.out.println("subodh");
+        System.out.println("patil");
+    }
+}
