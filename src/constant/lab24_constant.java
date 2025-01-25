38dc16242217e69223f4901e6a93a660a2f52000
subodh2499 <sdpatil2405@gmail.com>
2025-01-25 15:37:22 +0530

    practice

---
+++ src/constant/lab24_constant.java
@@ -0,0 +1,14 @@
+package constant;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class lab24_constant {
+    public static void main(String[] args) {
+
+        // Literal
+        int adee = 65;
+        adee = 66;
+        System.out.println(adee);
+    }
+
+}
